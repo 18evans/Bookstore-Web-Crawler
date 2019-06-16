691f6af37138df7da224ac6581c7a40879a1c175
Alexander Kiderov <alexkiderov97@gmail.com>
2019-06-16 14:56:35 +0200

    responseReturnsServerErrorIfURLIsNullEmptyOrWhitespace 2/2 - check if URL is null, empty or White Space

--- src/main/java/rest/service/endpoint/WebCrawlerResource.java
+++ src/main/java/rest/service/endpoint/WebCrawlerResource.java
@@ -1,5 +1,7 @@
 package rest.service.endpoint;
 
+import org.apache.commons.lang3.StringUtils;
+
 import javax.inject.Singleton;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
@@ -16,16 +18,19 @@ public class WebCrawlerResource {
 
     /**
      * @param urlAsString - url of website you wish to crawl through
-     * @param type - the class name of the type of object you wish to look for.
-     *             See class names of the implementations of Result abstract class.
-     * @param keyword - the word you wish to look for in the title of the object type
+     * @param type        - the class name of the type of object you wish to look for.
+     *                    See class names of the implementations of Result abstract class.
+     * @param keyword     - the word you wish to look for in the title of the object type
      * @return - on success a collection of results in a JSONObject within a Response object
-     *          - on error JSONObject of a Response object containing the error message
+     * - on error JSONObject of a Response object containing the error message
      */
     @GET
     public Response getContent(@QueryParam("url") String urlAsString,
                                @QueryParam("type") @DefaultValue("") String type,
                                @QueryParam("keyword") @DefaultValue("") String keyword) {
+        if (StringUtils.isBlank(urlAsString)) {
+            return Response.serverError().build();
+        }
         return Response.ok().build();
     }
 

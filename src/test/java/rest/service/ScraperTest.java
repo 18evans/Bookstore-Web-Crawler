d1c8b480e5121388b74cda4330d6a206ab0d952c
Alexander Kiderov <alexkiderov97@gmail.com>
2019-06-18 05:16:44 +0200

    if page points to supported item assert NOT NULL - 1/?

--- src/test/java/rest/service/ScraperTest.java
+++ src/test/java/rest/service/ScraperTest.java
@@ -1,11 +1,18 @@
 package rest.service;
 
 import junitparams.JUnitParamsRunner;
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import rest.service.model.Item;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -15,13 +22,22 @@ import static org.mockito.Mockito.when;
 public class ScraperTest {
 
     private final Scraper scraper = new Scraper(); //SUT
+    /**
+     * Since URL is pointing to FHICT self service Web host required is Cisco VPN to connect.
+     */
+    private static URL exampleValidUrl;
+
+    @BeforeClass
+    public static void setUp() throws MalformedURLException {
+        exampleValidUrl = new URL("http://i367506.hera.fhict.nl/webcrawl_example/details.php?id=201");
+    }
 
     /**
      * If Jsoup {@link Document} object doesn't point to page of item,
      * returned will be null instance of Item.
      */
     @Test
-    public void onScrapeIfParameterPageDoesNotPointToItemReturnNull() {
+    public void ifPageIsNotOfAValidItemDoNotReturnNull() {
         //arrange
         Document document = mock(Document.class);
         when(document.body()).thenReturn(null); //document points to null page
@@ -33,4 +49,20 @@ public class ScraperTest {
         assertNull(result);
     }
 
+    /**
+     * If Jsoup {@link Document} object points to a page of a supported item category,
+     * returned will be  be null instance of Item.
+     */
+    @Test
+    public void ifPageIsAValidItemPageDoNotReturnNull() throws IOException {
+        //arrange - document point to valid webpage of an item
+        Document document = Jsoup.connect(exampleValidUrl.toString()).timeout(6000).get();
+
+        //act
+        Item result = scraper.scrapeAndGetItem(document);
+
+        //assert
+        assertNotNull(result);
+
+    }
 }

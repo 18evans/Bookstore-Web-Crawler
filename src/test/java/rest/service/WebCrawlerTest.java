78788de362fa9afbff8245963f7fdf0ce42d1e50
Alexander Kiderov <alexkiderov97@gmail.com>
2019-06-18 14:00:52 +0200

    clean up boilerplate code on Method used for ParameterizedTests

--- src/test/java/rest/service/WebCrawlerTest.java
+++ src/test/java/rest/service/WebCrawlerTest.java
@@ -13,11 +13,15 @@ import rest.service.model.Music;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.HashSet;
 import java.util.Set;
 
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.atLeast;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @RunWith(JUnitParamsRunner.class)
 public class WebCrawlerTest {
@@ -42,11 +46,11 @@ public class WebCrawlerTest {
         validUrl = new URL("https://fontys.nl");
     }
 
-    private Object[] crawlOneSiteToManySites() throws MalformedURLException {
-        return new Object[]{
-                new Object[]{new URL("http://i367506.hera.fhict.nl/webcrawl_example/details.php?id=101")},
-                new Object[]{new URL("http://i367506.hera.fhict.nl/webcrawl_example/details.php?id=201")},
-                new Object[]{new URL("http://i367506.hera.fhict.nl/webcrawl_example/details.php?id=301")}
+    private URL[] crawlOneSiteToManySites() throws MalformedURLException {
+        return new URL[]{
+                new URL("http://i367506.hera.fhict.nl/webcrawl_example/details.php?id=101"),
+                new URL("http://i367506.hera.fhict.nl/webcrawl_example/details.php?id=201"),
+                new URL("http://i367506.hera.fhict.nl/webcrawl_example/details.php?id=301")
         };
     }
 

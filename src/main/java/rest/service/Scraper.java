169be5532164c0ac1ec51e81efc1b5d864d74986
Alexander Kiderov <alexkiderov97@gmail.com>
2019-06-18 05:16:57 +0200

    if page points to supported item assert NOT NULL - 2/2

--- src/main/java/rest/service/Scraper.java
+++ src/main/java/rest/service/Scraper.java
@@ -1,11 +1,19 @@
 package rest.service;
 
 import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import rest.service.model.Books;
 import rest.service.model.Item;
 
 public class Scraper {
 
     public Item scrapeAndGetItem(Document document) {
-        return null;
+        Elements elements = document.select("div.media-details");
+
+        if (elements == null || elements.isEmpty()) {
+            return null; //not an item page
+        }
+
+        return new Books();
     }
 }
